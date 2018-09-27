abb11870a6314096be82887fc87fd48b25620240
songzhiheng <18079932230@163.com>
2018-09-27 10:46:47 +0800

    测试

--- src/main/java/myself/services/impl/IndexServiceImpl.java
+++ src/main/java/myself/services/impl/IndexServiceImpl.java
@@ -1,13 +1,16 @@
 package myself.services.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import myself.mapper.IndexMapper;
 import myself.services.IndexService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
+
 
 import java.util.List;
 
-@Service("indexService")
+@Component
+@Service
 public class IndexServiceImpl implements IndexService {
     @Autowired
     private IndexMapper indexMapper;
